a1a76fc4de2aba6b7fc2f1f5ec58fb9797ca5ed6
Guilherme Campano <campano.guilherm@gmail.com>
2023-03-26 23:08:44 +0100

    Fix Linked List

--- src/datastructures/singlyLinkedList.ts
+++ src/datastructures/singlyLinkedList.ts
@@ -1,31 +1,39 @@
 export class Node {
-  constructor(value) {
+  public value: number;
+  public next: Node | null;
+  constructor(value: number) {
     this.value = value;
     this.next = null;
   }
 }
 
 export class SinglyLinkedList {
+  public head: Node | null;
+  public tail: Node | null;
+  public length: number;
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
 
-  push(value) {
+  push(value: number): SinglyLinkedList | undefined {
     const node = new Node(value);
+    this.length += 1;
     if (!this.head) {
       this.head = node;
       this.tail = this.head;
-    } else {
+      return this;
+    }
+    if (this.tail) {
       this.tail.next = node;
       this.tail = node;
+      return this;
     }
-    this.length += 1;
-    return this;
+
   }
 
-  pop() {
+  pop(): Node | undefined {
     if (!this.head) return undefined;
     let currentNode = this.head;
     let newTail = currentNode;
@@ -44,7 +52,7 @@ export class SinglyLinkedList {
     return currentNode;
   }
 
-  shift() {
+  shift(): Node | undefined {
     if (!this.head) return undefined;
     const currentHead = this.head;
     this.head = this.head.next;
@@ -55,7 +63,7 @@ export class SinglyLinkedList {
     return currentHead;
   }
 
-  unshift(value) {
+  unshift(value: number): SinglyLinkedList | undefined {
     const node = new Node(value);
     if (!this.head) {
       this.head = node;
@@ -69,21 +77,22 @@ export class SinglyLinkedList {
     return this;
   }
 
-  get(index) {
+  get(index: number): Node | null {
     if (index < 0 || index > this.length - 1 || !this.head) {
       return null;
     }
 
-    let currentNode = this.head;
+    let currentNode: Node | null = this.head;
     while (index > 0) {
-      currentNode = currentNode.next;
+      currentNode = currentNode?.next || null;
       index--;
     }
     return currentNode;
   }
 
-  set(index, value) {
-    let node = this.get(index);
+  set(index: number, value: number): boolean {
+    if (index < 0 || index > this.length - 1) return false;
+    const node = this.get(index);
     if (!node) {
       return false;
     }
@@ -91,14 +100,16 @@ export class SinglyLinkedList {
     return true;
   }
 
-  insert(index, value) {
+  insert(index: number, value: number): boolean {
     if (index < 0 || index > this.length) return false;
     if (index === this.length) return !!this.push(value);
     if (index === 0) return !!this.unshift(value);
 
     const newNode = new Node(value);
     const previousNode = this.get(index - 1);
-    const nextNode = previousNode.next;
+    const nextNode = previousNode?.next || null;
+
+    if (!previousNode) return false;
 
     previousNode.next = newNode;
     newNode.next = nextNode;
@@ -106,101 +117,36 @@ export class SinglyLinkedList {
     return true;
   }
 
-  remove(index) {
-    if (index < 0 || index >= this.length) return null;
-    if (index === 0) return this.shift();
-    if (index === this.length - 1) return this.pop();
+  remove(index: number): Node | boolean {
+    if (index < 0 || index >= this.length) return false;
+    if (index === 0) return !!this.shift();
+    if (index === this.length - 1) return !!this.pop();
 
     const previousNode = this.get(index - 1);
-    const removedNode = previousNode.next;
-    previousNode.next = removedNode.next;
+    const removedNode = previousNode?.next;
+
+    if (!previousNode || !removedNode) return false;
+    previousNode.next = removedNode?.next;
+
     this.length--;
     return removedNode;
   }
 
-  reverse() {
-    let node = this.head;
+  reverse(): SinglyLinkedList | null {
+    if(!this.head) return null;
+    
+    let node: Node = this.head as Node;
     this.head = this.tail;
     this.tail = node;
-    let next;
-    let previous = null;
+    
+    let next: Node | null = null;
+    let previous: Node | null = null;
     for (let i = 0; i < this.length; i++) {
-      next = node.next;
+      next = node?.next || null;
       node.next = previous;
       previous = node;
-      node = next;
+      node = next as Node;
     }
     return this;
   }
 }
-/*
-
-100 -> 200 -> 300 -> 400 -> 500
-TAIL                       HEAD
-
-i=0
-null    100 -> 200 -> 300 -> 400 -> 500
-        TAIL                        HEAD
-PREV  <- NODE  <- NEXT
-
-i=1
-null    100 -> 200 -> 300 -> 400 -> 500
-        TAIL                        HEAD
-        PREV<- NODE<- NEXT
-
-
-
-
-[100 200 300 400 500]
-head = 100 head.next = 200
-tail = 500 tail.next = null
-----
-reverse()
-node = N100
-head = N500
-tail = N100
-next = undefined
-previous - null
-
-i=0
-next = N100.next = N200
-node.next = previous = null
-previous = N100
-node = N200
-
-N100 -> null
-N500 -> null
-
-
-i=1
-previous = N100
-node = N200
-
-next = N200.next = N300
-node.next = previous = N100
-N200 -> N100
-previous = N200
-next = N300
-
-N500 ... N200 -> N100 -> null
-
-i2
-N500 ... N300 -> N200 -> N100 -> null
-
-i3
-N500 ... N400 -> N300 -> N200 -> N100 -> null
-
-i4
-N500 -> N400 -> N300 -> N200 -> N100 -> null
-
-----
-100 -> 200 -> 300 -> 400 -> 500
-TAIL                       HEAD
-
-i=0
-
-100 -> 200 -> 300 -> 400 -> 500
-TAIL                        HEAD
-NODE
-
-*/
