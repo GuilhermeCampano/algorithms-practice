3d104231e54de8b951a01fad6eb7b18b925c5e19
Guilherme Campano <campano.guilherm@gmail.com>
2023-12-28 15:05:29 +0000

    Improve binary search tree tests

--- src/datastructures/binary-search-tree.test.ts
+++ src/datastructures/binary-search-tree.test.ts
@@ -5,84 +5,141 @@ function covertToNodeArray(nodes: Node[]): number[] {
 
 }
 describe('BinarySearchTree', () => {
-  let tree: BinarySearchTree;
-
-  beforeEach(() => {
-    tree = new BinarySearchTree();
-  });
-
   // INSERT
-  describe('GIVEN I have an empty tree', () => {
-    describe('WHEN I insert the node 1', () => {
-      beforeEach(() => tree.insert(1));
-      test('THEN the root should be 1', () => {
-        expect(tree.root?.value).toBe(1);
-      });
+  describe('GIVEN I have a new tree', () => {
+    let tree: BinarySearchTree;
+
+    beforeEach(() => {
+      tree = new BinarySearchTree();
     });
 
-    describe('WHEN I insert the node 2', () => {
+    describe('WHEN I insert the nodes 2, 1, and 3', () => {
       beforeEach(() => {
-        tree.insert(1);
         tree.insert(2);
-      });
-      test('THEN the root right should be 2', () => {
-        expect(tree.root?.right?.value).toBe(2);
-      });
-    });
-
-    describe('WHEN I insert the node 3', () => {
-      beforeEach(() => {
         tree.insert(1);
-        tree.insert(2);
         tree.insert(3);
       });
-      test('THEN the root right right should be 3', () => {
-        expect(tree.root?.right?.right?.value).toBe(3);
+
+      test('THEN the root should be 2', () => {
+        expect(tree.root?.value).toBe(2);
+      });
+
+      test('THEN the root left should be 1', () => {
+        expect(tree.root?.left?.value).toBe(1);
+      });
+
+      test('THEN the root right should be 3', () => {
+        expect(tree.root?.right?.value).toBe(3);
+      });
+    });
+
+    describe('WHEN I insert the nodes 5, 3, 7, 2, 4, 6, and 8', () => {
+      /**
+      *     5
+      *    / \
+      *   3   7
+      *  / \ / \
+      * 2  4 6  8
+      */
+      beforeEach(() => {
+        tree.insert(5);
+        tree.insert(3);
+        tree.insert(7);
+        tree.insert(2);
+        tree.insert(4);
+        tree.insert(6);
+        tree.insert(8);
+      });
+
+      test('THEN the root should be 5', () => {
+        expect(tree.root?.value).toBe(5);
+      });
+
+      test('THEN the root left should be 3 and root right should be 7', () => {
+        expect(tree.root?.left?.value).toBe(3);
+        expect(tree.root?.right?.value).toBe(7);
+      });
+
+      test('THEN the left child of root left should be 2 and the right child should be 4', () => {
+        expect(tree.root?.left?.left?.value).toBe(2);
+        expect(tree.root?.left?.right?.value).toBe(4);
+      });
+
+      test('THEN the left child of root right should be 6 and the right child should be 8', () => {
+        expect(tree.root?.right?.left?.value).toBe(6);
+        expect(tree.root?.right?.right?.value).toBe(8);
       });
     });
   });
 
   // FIND
-  describe('GIVEN I have a tree with [2,5,6] ', () => {
+  describe('GIVEN I have a tree with [2,5,6]', () => {
+    let tree: BinarySearchTree;
+
     beforeEach(() => {
+      tree = new BinarySearchTree();
       tree.insert(2);
       tree.insert(5);
       tree.insert(6);
     });
-    describe('WHEN I search for the node 5', () => {
-      test('THEN I should get the node with value 5', () => {
-        expect(tree.find(5)?.value).toBe(5);
-      });
+
+    test('WHEN I find the node with value 5 THEN it should return the node', () => {
+      const node = tree.find(5);
+      expect(node).not.toBeNull();
+      expect(node?.value).toBe(5);
     });
-    describe('WHEN I search for the node 7', () => {
-      test('THEN I should get null', () => {
-        expect(tree.find(7)).toBeNull();
-      });
+
+    test('WHEN I find the node with value 3 THEN it should return null', () => {
+      const node = tree.find(3);
+      expect(node).toBeNull();
+    });
+
+    test('WHEN I find the node with value 6 THEN it should return the node', () => {
+      const node = tree.find(6);
+      expect(node).not.toBeNull();
+      expect(node?.value).toBe(6);
     });
   });
 
-  // contains
-  describe('GIVEN I have a tree with [2,5,6] ', () => {
+  // CONTAINS
+  describe('GIVEN I have a tree with [2,5,6]', () => {
+    let tree: BinarySearchTree;
+
     beforeEach(() => {
+      tree = new BinarySearchTree();
       tree.insert(2);
       tree.insert(5);
       tree.insert(6);
     });
-    describe('WHEN I search for the node 5', () => {
-      test('THEN I should get true', () => {
-        expect(tree.contains(5)).toBe(true);
-      });
+
+    test('WHEN I check if the tree contains 5 THEN it should return true', () => {
+      const contains = tree.contains(5);
+      expect(contains).toBe(true);
     });
-    describe('WHEN I search for the node 7', () => {
-      test('THEN I should get false', () => {
-        expect(tree.contains(7)).toBe(false);
-      });
+
+    test('WHEN I check if the tree contains 3 THEN it should return false', () => {
+      const contains = tree.contains(3);
+      expect(contains).toBe(false);
+    });
+
+    test('WHEN I check if the tree contains 6 THEN it should return true', () => {
+      const contains = tree.contains(6);
+      expect(contains).toBe(true);
     });
   });
 
-  // BFS - breadth first search
+  // DFS and BFS TRAVERSAL
   describe('GIVEN I have a tree with [10,6,15,3,8,20]', () => {
+    /**
+    *     10
+    *    /  \
+    *   6   15
+    *  / \    \
+    * 3   8   20
+    */
+    let tree: BinarySearchTree;
     beforeEach(() => {
+      tree = new BinarySearchTree();
       tree.insert(10);
       tree.insert(6);
       tree.insert(15);
@@ -90,65 +147,36 @@ describe('BinarySearchTree', () => {
       tree.insert(8);
       tree.insert(20);
     });
-    describe('WHEN I traverse the tree', () => {
+
+    describe('WHEN I traverse with breadth first search (BFS) ', () => {
       test('THEN I should get [10,6,15,3,8,20]', () => {
         const result = covertToNodeArray(tree.bfs());
         expect(result).toEqual([10, 6, 15, 3, 8, 20]);
       });
     });
-  });
-
-  // DFS - depth first search
-  describe('GIVEN I have a tree with [10,6,15,3,8,20]', () => {
-    beforeEach(() => {
-      tree.insert(10);
-      tree.insert(6);
-      tree.insert(15);
-      tree.insert(3);
-      tree.insert(8);
-      tree.insert(20);
-    });
-    describe('WHEN I traverse the tree', () => {
+    describe('WHEN I traverse with depth first search (DFS)', () => {
       test('THEN I should get [10,6,3,8,15,20]', () => {
         const result = covertToNodeArray(tree.dfsPreOrder());
         expect(result).toEqual([10, 6, 3, 8, 15, 20]);
       });
     });
-  });
 
-  // DFS - depth first search - post order
-  describe('GIVEN I have a tree with [10,6,15,3,8,20]', () => {
-    beforeEach(() => {
-      tree.insert(10);
-      tree.insert(6);
-      tree.insert(15);
-      tree.insert(3);
-      tree.insert(8);
-      tree.insert(20);
-    });
-    describe('WHEN I traverse the tree', () => {
+    describe('WHEN I traverse with depth first search  (Post order)', () => {
       test('THEN I should get [3,8,6,20,15,10]', () => {
         const result = covertToNodeArray(tree.dfsPostOrder());
         expect(result).toEqual([3, 8, 6, 20, 15, 10]);
       });
     });
-  });
 
-  // DFS - depth first search - in order
-  describe('GIVEN I have a tree with [10,6,15,3,8,20]', () => {
-    beforeEach(() => {
-      tree.insert(10);
-      tree.insert(6);
-      tree.insert(15);
-      tree.insert(3);
-      tree.insert(8);
-      tree.insert(20);
-    });
-    describe('WHEN I traverse the tree', () => {
+    describe('WHEN I traverse with depth first search (In order)', () => {
       test('THEN I should get [3,6,8,10,15,20]', () => {
         const result = covertToNodeArray(tree.dfsInOrder());
         expect(result).toEqual([3, 6, 8, 10, 15, 20]);
       });
     });
+
   });
+
+
+
 });
--- src/datastructures/binary-search-tree.ts
+++ src/datastructures/binary-search-tree.ts
@@ -17,7 +17,21 @@ export class BinarySearchTree {
     this.root = null;
   }
 
-  insert(val: number): BinarySearchTree | undefined {
+  /**
+   * When inserting a node, the value of the node is compared to the value of the current node
+   * If the value is less than the current node, the value is inserted to the left of the current node
+   * If the value is greater than the current node, the value is inserted to the right of the current node
+   * 
+   * For example, inserting the values 5, 3, and 7 in that order would result in the following tree:
+   *  5
+   * / \
+   * 3  7
+   * ---------------------------------------
+   * Time complexity: O(log n)
+   * This is because the tree is divided in half each time a node is compared, for instance:
+   * a tree with 16 nodes will take 4 steps to find the node (log2(16) = 4 or 2^4 = 16)
+   */
+  insert(val: number): BinarySearchTree {
     const newNode = new Node(val);
     if (!this.root) {
       this.root = newNode;
@@ -25,7 +39,7 @@ export class BinarySearchTree {
     }
     let current: Node | null = this.root;
     while (current) {
-      if (val === current.value) return undefined;
+      if (val === current.value) return this;
       if (val < current.value) {
         if (!current.left) {
           current.left = newNode;
@@ -76,6 +90,22 @@ export class BinarySearchTree {
     return false;
   }
 
+  /**
+   * Breadth-First Search (BFS)
+   * The algorithm begins at the root of the tree,
+   * and explores all its neighboring nodes at the current depth level before progressing to nodes at the next depth level.
+   *
+   * Consider the following binary tree:
+   *     10
+   *    /  \
+   *   6   15
+   *  / \    \
+   * 3   8   20
+   *          \
+   *          30
+   * Therefore, the BFS traversal of this tree would be:
+   * [10, 6, 15, 3, 8, 20, 30]
+   */
   bfs(): Node[] {
     const data: Node[] = [];
     const queue: Node[] = [];
@@ -90,6 +120,21 @@ export class BinarySearchTree {
     return data;
   }
 
+  /**
+   * Depth-First Search (DFS) - Pre Order
+   * The algorithm starts at the root node and explores as far as possible along each branch before backtracking.
+   * For example, the following tree:
+   *     10
+   *    /  \
+   *   6   15
+   *  / \    \
+   * 3   8   20
+   *          \
+   *          30
+   *
+   * Would be traversed in the following order:
+   * [10, 6, 3, 8, 15, 20, 30]  
+   */
   dfsPreOrder(): Node[] {
     const data: Node[] = [];
     const current = this.root;
@@ -104,6 +149,21 @@ export class BinarySearchTree {
     return data;
   }
 
+  /**
+   * Depth-First Search (DFS) - Post Order
+   * Similar to DFS Pre Order, except the root node is visited last.
+     For example, the following tree:
+   *     10
+   *    /  \
+   *   6   15
+   *  / \    \
+   * 3   8   20
+   *          \
+   *          30
+   *
+   * Would be traversed in the following order:
+   * [3, 8, 6, 30, 20, 15, 10]
+   **/
   dfsPostOrder(): Node[] {
     const data: Node[] = [];
     const current = this.root;
@@ -118,6 +178,21 @@ export class BinarySearchTree {
     return data;
   }
 
+  /**
+   * Depth-First Search (DFS) - In Order
+   * Similar to DFS Pre Order, except the root node is visited in the middle.
+   * For example, the following tree:
+   *     10
+   *    /  \
+   *   6   15
+   *  / \    \
+   * 3   8   20
+   *          \
+   *          30
+   * 
+   * Would be traversed in the following order:
+   * [3, 6, 8, 10, 15, 20, 30]
+   */
   dfsInOrder(): Node[] {
     const data: Node[] = [];
     const current = this.root;
@@ -131,5 +206,5 @@ export class BinarySearchTree {
     traverse(current);
     return data;
   }
-  
+
 }
