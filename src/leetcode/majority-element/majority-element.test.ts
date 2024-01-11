bf7a35cdf719a7595932e49d0f02d26456fedd77
Guilherme Campano <campano.guilherm@gmail.com>
2024-01-11 20:52:53 +0000

    Majority element alternative solution

--- src/leetcode/majority-element/majority-element.test.ts
+++ src/leetcode/majority-element/majority-element.test.ts
@@ -1,20 +1,23 @@
-import {majorityElement} from './majority-element';
+import {majorityElement, boyerMooreAlgorithm} from './majority-element';
 
 describe('Majority Element', () => {
 
   it('should return 3 when nums = [3,2,3]', () => {
     const nums = [3,2,3];
     expect(majorityElement(nums)).toBe(3);
+    expect(boyerMooreAlgorithm(nums)).toBe(3);
   });
 
   it('should return 2 when nums = [2,2,1,1,1,2,2]', () => {
     const nums = [2,2,1,1,1,2,2];
     expect(majorityElement(nums)).toBe(2);
+    expect(boyerMooreAlgorithm(nums)).toBe(2);
   });
 
   it('should return 1 when nums = [1]', () => {
     const nums = [1];
     expect(majorityElement(nums)).toBe(1);
+    expect(boyerMooreAlgorithm(nums)).toBe(1);
   });
 
   it('should return 1 when nums = [1,1,1,1,1,2]', () => {
--- src/leetcode/majority-element/majority-element.ts
+++ src/leetcode/majority-element/majority-element.ts
@@ -7,6 +7,7 @@
  *
  * Solution: Frequency Counter
  * Time complexity: O(n), where n is the length of nums.
+ * Space complexity: O(n), where n is the length of nums.
  * The algorithm stops iterating when the frequency of a number is greater than half of the length of the list.
  */
 
@@ -26,4 +27,31 @@ export function majorityElement(nums: number[]): number {
   }
 
   return -1; // Not found
+}
+
+
+/**
+ * Boyer-Moore Voting Algorithm (for Majority Element problem):
+ * Time complexity: O(n), where n is the length of nums.
+ * Space complexity: O(1).
+ * - Not the same as Boyer-Moore string search algorithm.
+ * - Maintains a count of the current candidate for the majority element.
+ * - Iterates through the array:
+ *   - If count is 0, sets current element as candidate.
+ *   - If current element is same as candidate, increments count.
+ *   - If current element is different from candidate, decrements count.
+ * - Majority element will survive this process and be the candidate at the end.
+ */
+export function boyerMooreAlgorithm(numbers: number[]): number {
+  let count = 0;
+  let candidate = 0;
+
+  for (const number of numbers) {
+    if (count === 0) {
+      candidate = number;
+    }
+    count += (number === candidate) ? 1 : -1;
+  }
+
+  return candidate;
 }
